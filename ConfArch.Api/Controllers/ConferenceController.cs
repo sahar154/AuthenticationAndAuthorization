b13b5bc309768465a3f99eec02c136b9e0ff9da4
Sahar.unq <sahar@unq.co.il>
2023-11-15 12:22:39 +0200

    add to github

---
+++ ConfArch.Api/Controllers/ConferenceController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ConfArch.Data.Models;
+using ConfArch.Data.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConfArch.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    //[AllowAnonymous]
+    public class ConferenceController : Controller
+    {
+        private readonly IConferenceRepository repo;
+
+        public ConferenceController(IConferenceRepository repo)
+        {
+
+            this.repo = repo;
+        }
+
+        public async Task<IEnumerable<ConferenceModel>> GetAll()
+        {
+            return await repo.GetAll();
+        }
+
+        [HttpPost]
+        public void Add(ConferenceModel conference)
+        {
+            repo.Add(conference);
+        }
+    }
+}
